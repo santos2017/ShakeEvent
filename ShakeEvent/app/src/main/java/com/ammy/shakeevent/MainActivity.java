f43cbd6d5e8db8c7bd803c2e2ba6c6dd8b765a7e
santos2017 <santosh.303lo@yahoo.com>
2017-01-09 20:53:42 +0530

    Add files via upload
    Kick the screen alive by shaking it as my power button is broken.
    Used code for the shake event from http://androidammy.blogspot.in/2015/05/accelerometer-shake-events-example-with.html

---
+++ ShakeEvent/app/src/main/java/com/ammy/shakeevent/MainActivity.java
@@ -0,0 +1,23 @@
+package com.ammy.shakeevent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.ammy.shakeevent.R;
+
+public class MainActivity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		Intent intent = new Intent(this, MyService.class);
+		//Start Service
+		startService(intent);
+	}
+
+}
